849c860585badcbc0f009839ef15f68786a8bf70
Josue Lopez <josue@MacBook-Pro-de-Josue.local>
2018-12-11 11:54:08 -0600

    Updates

---
+++ precache-manifest.565dd0149217f484d2fa747cfd1a50cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f85670585e6995e1ae92e090c538a133",
+    "url": "/issue-app/static/media/issue.f8567058.svg"
+  },
+  {
+    "revision": "a3ab788ab73d2154ecb6",
+    "url": "/issue-app/static/js/runtime~main.a3ab788a.js"
+  },
+  {
+    "revision": "34c58198c116ab200172",
+    "url": "/issue-app/static/js/main.34c58198.chunk.js"
+  },
+  {
+    "revision": "4ea10408f04e7a42a36e",
+    "url": "/issue-app/static/js/1.4ea10408.chunk.js"
+  },
+  {
+    "revision": "34c58198c116ab200172",
+    "url": "/issue-app/static/css/main.412894bc.chunk.css"
+  },
+  {
+    "revision": "75c28f18e9e8251719186d8ad92d57d5",
+    "url": "/issue-app/index.html"
+  }
+];
